5940532f881f3b8cd6c2c1b3a5bc268ae115c6f3
dev-Anamul <ah9737677@gmail.com>
2022-02-01 21:51:59 +0600

    update app.js

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 function qdocument(data) {
-    return document.querySelector(data)
+  return document.querySelector(data);
 }
 
-qdocument('.s')
-
+qdocument('.s');
 
 setInterval(() => {
-    let date = new Date();
+  d = new Date();
+  hr = d.getHours();
+  min = d.getMinutes();
+  sec = d.getSeconds();
 
-    let secend = date.getSeconds() * 6;
-    let munit = date.getMinutes() * 6;
-    let hours = (date.getHours() * 30) + (munit / 12);
+  hr_rotation = 30 * hr + min / 2;
+  min_rotation = 6 * min + sec / 10;
+  sec_rotation = 6 * sec;
 
-    qdocument('.s').style.transform = `rotate(${secend}deg)`
-    qdocument('.m').style.transform = `rotate(${munit}deg)`
-    qdocument('.h').style.transform = `rotate(${hours}deg)`
-})
+  qdocument('.s').style.transform = `rotate(${sec_rotation}deg)`;
+  qdocument('.m').style.transform = `rotate(${min_rotation}deg)`;
+  qdocument('.h').style.transform = `rotate(${hr_rotation}deg)`;
+});
